03277f6b9eab3aca8f92e87c2f7ca066bded1bca
jm-essis <nessis@byupthway.edu>
2026-05-09 23:11:43 +0000

    Got It

---
+++ scripts/getdates.js
@@ -0,0 +1,6 @@
+// Get current year
+const year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+
+// Get last modified date
+document.getElementById("lastModified").textContent = "Last Modified: " + document.lastModified;
